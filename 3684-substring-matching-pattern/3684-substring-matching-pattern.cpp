9683185aeb0c3c422f12e60aca5c0b34be3ff9cf
Abhinav4317 <143246854+Abhinav4317@users.noreply.github.com>
2025-01-06 11:44:15 +0530

    Time: 2 ms (60%), Space: 8.6 MB (100%) - LeetHub

--- 3684-substring-matching-pattern/3684-substring-matching-pattern.cpp
+++ 3684-substring-matching-pattern/3684-substring-matching-pattern.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    /**
+        class Solution {
+public:
+    bool hasMatch(string s, string p) {
+        int x = p.find("*");
+        string b = p.substr(0, x);
+        string e = p.substr(x + 1);
+        int i = s.find(b);
+        int j = s.rfind(e);
+        return i != -1 && j != -1 && i + b.size() <= j;
+    }
+};
+    */
     bool hasMatch(string s, string p) {
         int n1=s.size(),n2=p.size();
         if(n2-1>n1) return false;
