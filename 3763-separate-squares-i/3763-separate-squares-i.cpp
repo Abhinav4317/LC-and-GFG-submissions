47acd51e90ac46013e60e3097395a1b1dd82dcc3
Abhinav Gangwar <143246854+Abhinav4317@users.noreply.github.com>
2025-02-19 12:15:46 +0530

    Time: 112 ms (55.82%), Space: 232.3 MB (5%) - LeetHub

--- 3763-separate-squares-i/3763-separate-squares-i.cpp
+++ 3763-separate-squares-i/3763-separate-squares-i.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
-    double solve(double pos,vector<vector<int>>& squares){
-        int n=squares.size();
-        double ab=0,bel=0;
-        for(int i=0;i<n;i++){
-            int x = squares[i][0], y = squares[i][1];
-            int l = squares[i][2];
-            double total = (double) l * l;
-
-            if(pos<=y) ab+=total;
-            else if(pos>=y+l) bel+=total;
-            else{
-                double ahi = (y + l) - pos;
-                double bhi = pos - y;
-                ab += l * ahi;
-                bel += l * bhi;
-            } 
-        }
-        return ab-bel;
-    }
     double separateSquares(vector<vector<int>>& squares) {
-        double low=0,high=2e9;
-        double lim=1.0e-5;
-        while(high-low>=lim){
-            double mid=(low+high)/2.0;
-            double diff=solve(mid,squares);
-            if(diff>0) low=mid;
-            else high=mid;
+        double total_area = 0;
+        for (const auto& square : squares) {
+            total_area += (double)square[2] * (double)square[2];
         }
-        return high;
+
+        vector<pair<int, pair<int, int>>> events;
+        for (const auto& square : squares) {
+            int x = square[0], y = square[1], l = square[2];
+            events.push_back({y, {1, l}});       // Start of square
+            events.push_back({y + l, {0, l}});  // End of square
+        }
+
+        // Sort events by y-coordinate
+        sort(events.begin(), events.end());
+
+        double combined_width = 0;
+        double curr_area = 0;
+        int prev_height = 0;
+
+        for (int i = 0; i < events.size(); i++) {
+            int y = events[i].first;
+            int start = events[i].second.first;
+            int l = events[i].second.second;
+
+            double height_diff = y - prev_height;
+            double area_diff = combined_width * height_diff;
+
+            if (curr_area + area_diff >= total_area / 2) {
+                // Calculate the optimal height difference
+                double optimal_height_diff = (total_area / 2 - curr_area) / combined_width;
+                return prev_height + optimal_height_diff;
+            }
+
+            combined_width += start ? l : -l;
+            curr_area += area_diff;
+            prev_height = y;
+        }
+
+        return -1; // This should never be reached
     }
 };
